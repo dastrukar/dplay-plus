ec970f267759cfa0a0f92d86131808375f9599b3
dastrukar <3derid@gmail.com>
2021-02-20 02:12:22 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+// include stuff
+#include "parser.cpp"
+
+// headers
+#include "parser.h"
+
+
+int main()
+{
+    std::cout << add(1, 2);
+    return 0;
+}
---
+++ parser.cpp
@@ -0,0 +1,11 @@
+#include "parser.h"
+#include <iostream>
+#include <fstream>
+
+/*
+ * How does this work????
+ */
+int add(int x, int y)
+{
+    return x + y;
+}
