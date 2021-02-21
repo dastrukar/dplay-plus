0a3ebc4e33c921ed1d446cd0e976ef316f62e0fa
dastrukar <3derid@gmail.com>
2021-02-21 15:54:12 +0800

    Parser: Add more functions

--- parser.cpp
+++ parser.cpp
@@ -13,3 +13,37 @@ bool check_comment(std::string text)
 
     return false;
 }
+
+
+/*
+ * Returns true, if there's a : at the start of the line
+ */
+bool check_preset(std::string text)
+{
+    if (text[0] == ':') { return true; }
+
+    return false;
+}
+
+
+/*
+ * Returns true, if there's a $ at the start of the line
+ */
+bool check_variable(std::string text)
+{
+    if (text[0] == '$') { return true; }
+
+    return false;
+}
+
+
+std::string get_variable(std::string text)
+{
+    if (check_variable(text))
+    {
+        std::string variable;
+        for(int i = 1; i < 6; i++) { variable += text[i]; }
+
+        return variable;
+    }
+}
--- parser.h
+++ parser.h
@@ -1,3 +1,7 @@
 #include <string>
 
 bool check_comment(std::string text); 
+bool check_preset(std::string text);
+bool check_variable(std::string text);
+
+std::string get_variable(std::string text);
